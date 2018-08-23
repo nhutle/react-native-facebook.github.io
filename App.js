ee925b343eebfa878b452fd408704f4d8df2f6e7
Nhut Le <leminhnhut08t1@gmail.com>
2018-08-23 18:44:19 +0700

    touchables

--- App.js
+++ App.js
@@ -1,38 +1,43 @@
 import React, { Component } from 'react';
-import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+import { Alert, AppRegistry, Platform, StyleSheet, Text, TouchableHighlight,
+  TouchableOpacity, TouchableNativeFeedback,  TouchableWithoutFeedback, View } from 'react-native';
 
-export default class ButtonBasics extends Component {
+export default class Touchables extends Component {
   _onPressButton() {
     Alert.alert('You tapped the button!')
+  }
+  
+  _onLongPressButton() {
+    Alert.alert('You long-pressed the button!');
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.buttonContainer}>
-          <Button
+        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>TouchableHighlight</Text>
+            </View>
+        </TouchableHighlight>
+        <TouchableOpacity onPress={this._onPressButton}>
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>TouchableOpacity</Text>
+            </View>
+        </TouchableOpacity>
+        <TouchableNativeFeedback 
             onPress={this._onPressButton}
-            title="Press Me"
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-            color="#841584"
-          />
-        </View>
-        <View style={styles.alternativeLayoutButtonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="This looks great!"
-          />
-          <Button
-            onPress={this._onPressButton}
-            title="OK!"
-            color="#841584"
-          />
-        </View>
+            background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}
+        >
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
+            </View>
+        </TouchableNativeFeedback>
+        <TouchableWithoutFeedback onPress={this._onPressButton}>
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
+            </View>
+        </TouchableWithoutFeedback>
+        
       </View>
     );
   }
